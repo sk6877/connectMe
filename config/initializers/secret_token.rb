22fa731d0ad0bd244deba02939712e1724d503a5
sk6877 <subbiah.nitt@gmail.com>
2013-05-09 06:25:21 -0700

    With working Home page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConnectMe::Application.config.secret_token = '65fd897de05cc84147ff5176afb6ea820bd9e6ad8cc7e88ebe4253e9372bc798da01de461729694cfe0d412aeab67e4350e6a6a0ea9f94a7bd9f6bd11c87daa0'
