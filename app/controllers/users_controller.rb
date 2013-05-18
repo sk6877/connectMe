76151eaf14595c0b30a70415fffb09cb17a814ba
sk6877 <subbiah.nitt@gmail.com>
2013-05-18 11:55:29 -0700

    Finish user edit, update, index, destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update, :index]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user, only: [:destroy]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -15,6 +23,49 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end 
+
+  def update
+    @user = User.find(params[:id])
+    passwd = params[:user][:password]
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    flash[:success] = "User destroyed"
+    redirect_to users_path
+  end
+ private
+
+  def signed_in_user
+    if(! signed_in? )
+      flash[:notice] = "Please sign in"
+      redirect_to signin_path
+    end
+  end  
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+  
+  def admin_user
+    if(!current_user.admin?)
+      flash[:notice] = "You must be the Admin"
+      redirect_to root_path
     end
   end
 
